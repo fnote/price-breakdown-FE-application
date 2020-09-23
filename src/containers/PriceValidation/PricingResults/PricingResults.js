bd53cfbc9acd4f10c17ef7cef595abbe7e1c8502
Michael Lu <milu8609@sysco.com>
2020-09-23 14:33:30 +0530

    Updated custom icons

--- src/containers/PriceValidation/PricingResults/PricingResults.js
+++ src/containers/PriceValidation/PricingResults/PricingResults.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function PricingResults() {
-  return (
-    <div className="pricing-results">
-     Pricing Result
-    </div>
-  );
+  return <div className="pricing-results">Pricing Result</div>;
 }
 
 export default PricingResults;
--- src/containers/PriceValidation/SearchPanel/SearchPanel.js
+++ src/containers/PriceValidation/SearchPanel/SearchPanel.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function SearchPanel() {
-  return (
-    <div className="searchpanel">
-     Search
-    </div>
-  );
+  return <div className="searchpanel">Search</div>;
 }
 
 export default SearchPanel;
