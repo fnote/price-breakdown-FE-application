1127f5ccb61bf94c649abc6b0b82b02bdef23e3b
Michael Lu <milu8609@sysco.com>
2020-09-29 18:03:05 +0530

    Added mobile app bar

--- src/components/AppBar/AccountWidget.js
+++ src/components/AppBar/AccountWidget.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+import {
+  MenuOutlined,
+  CloseOutlined,
+  QuestionCircleOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import Navigation from "./Navigation";
 class AccountWidget extends React.Component {
   state = {
-    visible: false
+    visible: false,
   };
   logoutButtonClicked = async () => {
     // await Auth.signOut();
@@ -12,7 +18,7 @@ class AccountWidget extends React.Component {
     this.setState({ visible: !visible });
   };
 
-  changeLanguage = lng => {
+  changeLanguage = (lng) => {
     const { i18n } = this.props;
     i18n.changeLanguage(lng);
   };
@@ -29,11 +35,10 @@ class AccountWidget extends React.Component {
       <div className={visible ? "account-widget open" : "account-widget"}>
         <div
           id="user-widget"
-          className={visible ? "user-widget show" : "user-widget"}
-        >
+          className={visible ? "user-widget show" : "user-widget"}>
           <div className="user">
             <div className="name">
-                milu8609
+              milu8609
               <div className="welcome">Signed In</div>
             </div>
           </div>
@@ -43,9 +48,8 @@ class AccountWidget extends React.Component {
             role="button"
             tabIndex="0"
             onKeyPress={this.toggleMenu}
-            onClick={this.toggleMenu}
-          >
-            <div className="pic"/>
+            onClick={this.toggleMenu}>
+            <div className="pic" />
             <span className="menuicon fi flaticon-next " />
           </div>
         </div>
@@ -57,10 +61,10 @@ class AccountWidget extends React.Component {
             role="button"
             tabIndex="-1"
             onKeyPress={() => {}}
-            onClick={this.toggleMenu}
-          >
+            onClick={this.toggleMenu}>
+            <Navigation />
             <ul>
-            <li className="hide">
+              <li className="hide">
                 <div className="menulabel">Profile</div>
                 <span className="icon fi flaticon-account" />
               </li>
@@ -91,12 +95,12 @@ class AccountWidget extends React.Component {
                 <span className="icon fi flaticon-user" />
               </li>
               <li>
+                <QuestionCircleOutlined className="icon" />
                 <div className="menulabel">Help &amp; Training</div>
-                <span className="icon fi flaticon-question" />
               </li>
               <li onClick={() => this.logoutButtonClicked()}>
+                <LogoutOutlined className="icon" />
                 <div className="menulabel">Logout</div>
-                <span className="icon fi flaticon-logout" />
               </li>
             </ul>
           </div>
@@ -110,23 +114,23 @@ class AccountWidget extends React.Component {
             onClick={this.toggleMenu}
           />
         )}
-        <div
-          role="button"
-          tabIndex="-4"
-          onKeyPress={() => {}}
-          className="sidemenu-toggle"
-          onClick={this.toggleMenu}
-        >
-          <i className="fi flaticon-menu"/>
-        </div>
+        {!visible && (
+          <div
+            role="button"
+            tabIndex="-4"
+            onKeyPress={() => {}}
+            className="sidemenu-toggle"
+            onClick={this.toggleMenu}>
+            <MenuOutlined />
+          </div>
+        )}
         <div
           role="button"
           tabIndex="-5"
           onKeyPress={() => {}}
           className={visible ? "sidemenu-closer show" : "sidemenu-closer"}
-          onClick={this.toggleMenu}
-        >
-          <i className="fi flaticon-close"/>
+          onClick={this.toggleMenu}>
+          <CloseOutlined />
         </div>
       </div>
     );
