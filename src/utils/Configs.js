04761b5bf0c275304cc5d1cfd6a5f06f75c4cc5c
adis0892 <amoda.dissanayake@sysco.com>
2020-10-19 10:54:19 -0500

    local urls updated use local env

--- src/utils/Configs.js
+++ src/utils/Configs.js
@@ -13,15 +13,15 @@ export const getBffUrlConfig = () => {
     } else if (process.env.REACT_APP_ENV === 'exe') {
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/logout';
-        CONFIG.userDetailsUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/user-details';
+        CONFIG.userDetailsUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1˚/pci-bff/auth/user-details';
     } else if (process.env.REACT_APP_ENV === 'prod') {
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-prod.prcp.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'https://cloud-pci-bff-prod.prcp.us-east-1.aws.sysco.net/v1/pci-bff/auth/logout';
         CONFIG.userDetailsUrl = 'https://cloud-pci-bff-prod.prcp.us-east-1.aws.sysco.net/v1/pci-bff/auth/user-details';
     } else {
-        CONFIG.loginRedirectionUrl = 'http://localhost:4000/exe/v1/pci-bff/auth/login';
-        CONFIG.logOutRedirectionUrl = 'http://localhost:4000/exe/v1/pci-bff/auth/logout';
-        CONFIG.userDetailsUrl = 'http://localhost:4000/exe/v1/pci-bff/auth/user-details';
+        CONFIG.loginRedirectionUrl = 'http://localhost:4000/local/v1/pci-bff/auth/login';
+        CONFIG.logOutRedirectionUrl = 'http://localhost:4000/local/v1/pci-bff/auth/logout';
+        CONFIG.userDetailsUrl = 'http://localhost:4000/local˚/v1/pci-bff/auth/user-details';
 
     }
     return CONFIG;
