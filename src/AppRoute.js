1d19d71a651a4666bc03258d7a895d69f10cc629
Sanjaya Amarasinghe <sanjaya.amarasinghe@trycake.com>
2020-10-22 16:40:05 +0530

    Remove redux usages

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -1,14 +1,11 @@
 import React, {Fragment} from 'react';
-import {Provider} from "react-redux";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ApplicationBase from './containers/ApplicationBase';
-import store from './reducers';
 import UserDetailContextProvider from './containers/UserDetailContext';
 import AppLoaderContextProvider from './components/AppLoderContext';
 
 function AppRoute() {
   return (
-      <Provider store={store}>
         <Fragment>
           <Router>
                   <Switch>
@@ -20,7 +17,6 @@ function AppRoute() {
                   </Switch>
           </Router>
         </Fragment>
-      </Provider>
   );
 }
 
