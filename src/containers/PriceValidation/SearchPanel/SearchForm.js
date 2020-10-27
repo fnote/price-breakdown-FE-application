26c19f246bd6c7b38e6871051aede14a11f278f6
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-10-27 11:28:14 +0530

    [PRCP-2491] Fix null reference issue

--- src/containers/PriceValidation/SearchPanel/SearchForm.js
+++ src/containers/PriceValidation/SearchPanel/SearchForm.js
@@ -38,7 +38,7 @@ const formRequestBody = (requestData) => {
 const SearchForm = () => {
     const priceValidationContext = useContext(PriceValidationContext);
     const userDetailContext = useContext(UserDetailContext);
-    const { userDetails: { businessUnitMap } } = userDetailContext.userDetailsData;
+    const { userDetails: { businessUnitMap = new Map() } } = userDetailContext.userDetailsData;
 
     const onSubmit = (values) => {
         priceValidationContext.setIsLoading(true);
