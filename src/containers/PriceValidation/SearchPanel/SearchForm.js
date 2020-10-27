08b3eaae67cef8868a9e7f769c38d72091a9568f
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-10-27 13:03:26 +0530

    Revert "Debugging 2"
    This reverts commit 1997f3afc51824c1ee3821964f5b64ee53d18422.

--- src/containers/PriceValidation/SearchPanel/SearchForm.js
+++ src/containers/PriceValidation/SearchPanel/SearchForm.js
@@ -38,7 +38,7 @@ const formRequestBody = (requestData) => {
 const SearchForm = () => {
     const priceValidationContext = useContext(PriceValidationContext);
     const userDetailContext = useContext(UserDetailContext);
-    const { userDetailsData: { businessUnitMap = new Map() } } = userDetailContext;
+    const { userDetails: { businessUnitMap = new Map() } } = userDetailContext.userDetailsData;
 
     const onSubmit = (values) => {
         priceValidationContext.setIsLoading(true);
