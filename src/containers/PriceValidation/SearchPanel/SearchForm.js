9ee4a76784afc3dde80ab9786bc03987b5cfea9a
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-11-02 11:31:22 +0530

    [PRCP-2491] Adding comments for explanation

--- src/containers/PriceValidation/SearchPanel/SearchForm.js
+++ src/containers/PriceValidation/SearchPanel/SearchForm.js
@@ -110,7 +110,7 @@ const SearchForm = () => {
               dropdownMatchSelectWidth={false}
               filterOption={(inputValue, option) => {
                 if (inputValue && option.children) {
-                  // creating pattern by escaping backslash(\), unless end up with a syntax error 
+                  // unless the backslash is escaped, this will end up with a syntax error 
                   const pattern = inputValue.replace(/\\/g, '').toLowerCase();
                   if (inputValue.length !== pattern.length || inputValue.match(/[^A-Za-z0-9 -]/)) {
                     return false;
