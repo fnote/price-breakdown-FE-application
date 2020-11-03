c659fe2be86bd688ebcfa5b1107ded0a5ca8a8c7
Sanjaya Amarasinghe <sanjaya.amarasinghe@trycake.com>
2020-11-03 20:10:39 +0530

    Fixing a failing test

--- src/utils/__tests__/PricingUtils.test.js
+++ src/utils/__tests__/PricingUtils.test.js
@@ -96,7 +96,11 @@ describe('getPriceUnit', () => {
 
 describe('generateDateObject', () => {
     test('should return the correct value', () => {
-        expect(generateDateObject('20201025').getTime()).toEqual(1603564200000);
+        const generatedDate = generateDateObject('20201025');
+        expect(generatedDate.getFullYear()).toEqual(2020);
+        // month is zero indexed
+        expect(generatedDate.getMonth() + 1).toEqual(10);
+        expect(generatedDate.getDate()).toEqual(25);
     });
 });
 
