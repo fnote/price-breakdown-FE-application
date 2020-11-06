2f42d9a776d9b417219a6d41533d53a1e306d9e1
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-11-06 12:27:20 +0530

    [RPCP-2110] Remove console logs

--- src/containers/FileUpload/DropZone.js
+++ src/containers/FileUpload/DropZone.js
@@ -10,7 +10,6 @@ const props = {
   onChange(info) {
     const { status } = info.file;
     if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
     }
     if (status === 'done') {
       message.success(`${info.file.name} file uploaded successfully.`);
