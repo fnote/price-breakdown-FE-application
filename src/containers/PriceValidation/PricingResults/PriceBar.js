9be983faf941aa0ace3fe41b47cc36150f81f752
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-11-06 10:15:17 +0530

    [PRCP-2110] Fix line length issues

--- src/containers/PriceValidation/PricingResults/PriceBar.js
+++ src/containers/PriceValidation/PricingResults/PriceBar.js
@@ -22,7 +22,8 @@ const renderPricePoint = ({
     </section>
 );
 
-const renderPricePoints = (priceBarMetaDataList, pricingData) => priceBarMetaDataList.map((pricePointMetaData) => renderPricePoint(pricePointMetaData, pricingData));
+const renderPricePoints = (priceBarMetaDataList, pricingData) => priceBarMetaDataList
+    .map((pricePointMetaData) => renderPricePoint(pricePointMetaData, pricingData));
 
 function PriceBar(props) {
   const { priceData: { pricePoints, product: { splitFlag, perWeightFlag, priceSourceName } } } = props;
