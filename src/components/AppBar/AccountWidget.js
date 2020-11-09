de6407110d3f32fdd77ed4d7b4256f400e112cbe
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-11-09 10:09:07 +0530

    [PRCP-2527] Remove unused imports

--- src/components/AppBar/AccountWidget.js
+++ src/components/AppBar/AccountWidget.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   MenuOutlined,
   CloseOutlined,
-  QuestionCircleOutlined,
   LogoutOutlined,
 } from '@ant-design/icons';
 import Navigation from './Navigation';
