a4f2d4a17b24e5c62e6ed8eeda72a8fdb9319d1c
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2020-11-16 16:34:58 +0530

    [PRCP-2572] Make whole area clickable navigation button

--- src/components/AppBar/Navigation.js
+++ src/components/AppBar/Navigation.js
@@ -8,16 +8,16 @@ function Navigation() {
   const match = useRouteMatch();
   return (
     <ul className="navigation">
-      <li className={getClassName(match, NAVIGATION_PATH_PRICE_VALIDATION)}>
-        <Link to={NAVIGATION_PATH_PRICE_VALIDATION}>
-          <i className="icon fi flaticon-accounting" />Pricing <span className="bold">Validation Tool</span>
-        </Link>        
-      </li>
-      <li className={getClassName(match, NAVIGATION_PATH_FILE_UPLOAD)}>
-        <Link to={NAVIGATION_PATH_FILE_UPLOAD}>
-          <i className="icon fi flaticon-cloud" />Calculations via <span className="bold">File Upload</span>
-        </Link>
-      </li>
+      <Link to={NAVIGATION_PATH_PRICE_VALIDATION}>
+        <li className={getClassName(match, NAVIGATION_PATH_PRICE_VALIDATION)}>          
+          <i className="icon fi flaticon-accounting" />Pricing <span className="bold">Validation Tool</span>                
+        </li>
+      </Link>  
+      <Link to={NAVIGATION_PATH_FILE_UPLOAD}>
+        <li className={getClassName(match, NAVIGATION_PATH_FILE_UPLOAD)}>          
+          <i className="icon fi flaticon-cloud" />Calculations via <span className="bold">File Upload</span>          
+        </li>
+      </Link>
     </ul>
   );
 }
