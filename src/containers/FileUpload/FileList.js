9d0a153169dda671169ed46d2c6c194c9a9c275c
adis0892 <amoda.dissanayake@sysco.com>
2020-11-24 18:14:21 -0600

    download for icon

--- src/containers/FileUpload/FileList.js
+++ src/containers/FileUpload/FileList.js
@@ -45,10 +45,15 @@ const columns = [
             <Button className="btn icon-only empty-btn">
               <i className="icon fi flaticon-bin" />
             </Button>
-            <Button className="btn icon-only empty-btn download-file">
+            <Button className="btn icon-only empty-btn download-file"
+                    onClick={() => {
+                      downloadFile('https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/C01C_066_000041_20201124_A.txt?AWSAccessKeyId=ASIAQRLXWZJ2LLLMGGFX&Expires=1606263175&Signature=3K0%2FWuloJxctFt7BtGP2I0XptwU%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEGgaCXVzLWVhc3QtMSJHMEUCIFG%2BBchlV48bGulCYeJqUTa3NFr0I%2BIKFlcBQzHIurtFAiEAwrFJf9zcD0D5gDRse%2By0k%2FV9GmFHEWfFSP4b%2F6%2FvLTsqlgMI4f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgwwMzcyOTUxNDc2MzYiDJ5Sj8JgC5EA%2BQjOpCrqAsdWZTqAyo7HvTtbFQmiOAhNizaOCCClV5dDuKzpyUCwqWjvWy4eWJPUnNUYKxPeaO%2FuYeBg%2BlfSVT8UNsKbLvKQNUtbDVQp4i1wu6%2FEqz2Yft1x6L2vuLtEYnmmJAOJEmAiFoYLoop8sU9hHt2ab1C4wkwkZfE2AvdFfgm9zQ3nEOy1meU9HUW0Wj8r4cbuZNmt2JaTrYkAPrNz1Tf8luSPZtLJxjPTTFsKQPcV51%2FsrHVOZoT0bo5wgK4Ap9dwY2BjSGm%2FDe9JI6doWMSEf2svfK3RtUtMKo34PQKx6V%2FE8Jm8qhpa24kSLPgqHww2qdzlD3oB31O32V0auAjp9kqz9aLE1L0BxAzDiXSPId7Ae4mZIJMJw7X1LRKf3nZQ729Dt%2FOk5qyZIHWbcZWcBmSYkpbWeI4OrbGYG0G0P18%2BVE6nvMYi5CbMqgDbIPmpW6XIQ%2BaPBMv6NXoNwbu6xF9KQbYf831oBfJGMOe79v0FOqYBZJsqnU9cvt%2BE3EAvfzGOqh%2F0iI8xnSy8lDDU%2Be1ewZ%2BANMpCG%2Fn6Z00D%2FV2Q%2Bpsr%2BKM7egs4hB7X7BaiyjirK3YOFbwbmhxN5CDaMLoKuogpLDDaQ0y%2BTvXZA4bPZl%2Fxg3x7cocCH%2F7pt2tBQVYzF%2BDv5Yo7yGB3%2F1mKbonM2H1mcmI2INOf8z9cI4mhaPbg1kg9BzovmJBf58kqdffF%2BfP%2BUqKddw%3D%3D')
+                    }}
+            >
               <i className="icon fi flaticon-cloud-computing" />
             </Button>
           </>
+
         ) : (
           <>
             <Button className="btn icon-only empty-btn cancel-process">
@@ -87,8 +92,26 @@ const fileListRequestHandler = () => fetch(getBffUrlConfig().listOutputFilesEndp
       'Accept': 'application/json, text/plain, */*',
       'Content-Type': 'application/json'
     },
-    credentials: 'include'
   }).then(handleResponse);
+
+
+const downloadFile  =  (url) => {
+  fetch(url, {
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+        },
+      }
+      )
+      .then((response) =>
+          console.log('Downloaded successfully'))
+      .catch(error => {
+        console.log('error in downloading', error.message);
+      })
+};
 
 class FileList extends React.Component {
   constructor(props) {
