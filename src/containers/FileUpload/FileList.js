4c98e80bc78efe3eb9ce0f1bea1da7f03cff089e
adis0892 <amoda.dissanayake@sysco.com>
2020-11-24 20:08:55 -0600

    updated s3 signed url hard corded

--- src/containers/FileUpload/FileList.js
+++ src/containers/FileUpload/FileList.js
@@ -102,7 +102,6 @@ const downloadFile  =  (url) => {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
         },
       }
       )
