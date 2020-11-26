84ef172c7f1bebcc5bbc99b3eccd409aa0f45901
iasa0862 <asanka.indunil@sysco.com>
2020-11-26 12:24:27 +0530

    file upload logic

--- src/containers/FileUpload/DropZone.js
+++ src/containers/FileUpload/DropZone.js
@@ -1,22 +1,87 @@
 import React from 'react';
-import { Upload, message } from 'antd';
+import {message, Upload} from 'antd';
+import {getBffUrlConfig} from "../../utils/Configs";
+import axios from 'axios';
 
-const { Dragger } = Upload;
+const {Dragger} = Upload;
+
+const formRequestBody = (fileName) => JSON.stringify({
+    fileNames: [fileName]
+});
+
+const handleResponse = (response) => response.json()
+    .then((json) => {
+        if (response.ok) {
+            return {success: true, data: json};
+        }
+        return {success: false, data: json};
+    });
+
+const fileUploadHandler = (payload) => {
+    fetch(getBffUrlConfig().fileUploadUrl, {
+        method: 'POST',
+        body: formRequestBody(payload.info.file.name),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+    })
+        .then(handleResponse)
+        .then((resp) => {
+            if (resp.success) {
+                const data = resp.data.data[0];
+                console.log(data.putUrl);
+                uploadArtifact(data.putUrl, payload.info)
+                    .then(result => {
+                        payload.onSuccess(result, payload.info.file);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+                return resp.data;
+            } else {
+                return null;
+            }
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+};
+
+const uploadArtifact = (path, payload) => {
+    const config = {
+        onUploadProgress: e => {
+            payload.onProgress({percent: (e.loaded / e.total) * 100});
+        },
+        headers: {'Content-Type': payload.file.type,  'x-amz-acl': 'public-read'},
+    };
+
+    return axios.put(path, payload.file, config);
+}
+
+const customUpload = info => {
+    const preSignedUrl = new Promise((resolve, reject) => fileUploadHandler({
+        info,
+        resolve,
+        reject,
+    }));
+};
 
 const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
+    name: 'file',
+    multiple: true,
+    customRequest: customUpload,
+    onChange(info) {
+        const {status} = info.file;
+        if (status !== 'uploading') {
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
 };
 
 function DropZone() {
--- src/utils/Configs.js
+++ src/utils/Configs.js
@@ -11,16 +11,19 @@ export const getBffUrlConfig = () => {
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/logout';
         CONFIG.userDetailsUrl = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/user-details';
+        CONFIG.fileUploadUrl = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/batch/signed-url/input';
     } else if (process.env.REACT_APP_ENV === 'exe') {
         CONFIG.priceDataEndpoint = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/pricing/pricing-data';
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/logout';
         CONFIG.userDetailsUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/user-details';
+        CONFIG.fileUploadUrl = 'https://cloud-pci-bff-exe.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/batch/signed-url/input';
     } else if (process.env.REACT_APP_ENV === 'stg') {
         CONFIG.priceDataEndpoint = 'https://cloud-pci-bff-stg.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/pricing/pricing-data';
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-stg.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'https://cloud-pci-bff-stg.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/logout';
         CONFIG.userDetailsUrl = 'https://cloud-pci-bff-stg.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/user-details';
+        CONFIG.fileUploadUrl = 'https://cloud-pci-bff-stg.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/batch/signed-url/input';
     } else if (process.env.REACT_APP_ENV === 'prod') {
         CONFIG.priceDataEndpoint = 'https://cloud-pci-bff-prod.prcp.us-east-1.aws.sysco.net/v1/pci-bff/pricing/pricing-data';
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-prod.prcp.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
