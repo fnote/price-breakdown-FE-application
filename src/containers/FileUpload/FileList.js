6c1bbd24fc75c37be44f29f516e5d45ed9d6c23f
adis0892 <amoda.dissanayake@sysco.com>
2020-12-03 15:38:17 -0600

    implemented single file download

--- src/containers/FileUpload/FileList.js
+++ src/containers/FileUpload/FileList.js
@@ -21,12 +21,12 @@ const columns = [
     dataIndex: 'action',
     className: 'action',
     width: 'auto',
-    render: (text) => (
+    render: (data) => (
       <div className="action-bar">
-        {text[0] === FILE_PROCESSING && (
+        {data.status === FILE_PROCESSING && (
           <div className="file-process-status">FILE IS BEING PROCESSED</div>
         )}
-        {text[0] === FILE_ERROR && (
+        {data.status === FILE_ERROR && (
           <div className="file-process-status error">
             File Contained errors
             <div className="divider"></div>
@@ -36,22 +36,20 @@ const columns = [
             </Button>
           </div>
         )}
-        {text[0] === FILE_SUCCESS && (
+        {data.status === FILE_SUCCESS && (
           <div className="file-process-status success">
             File processed successfully
           </div>
         )}
-        {text[0] !== FILE_PROCESSING ? (
+        {data.status !== FILE_PROCESSING ? (
           <>
             <Button className="btn icon-only empty-btn">
               <i className="icon fi flaticon-bin" />
             </Button>
             <Button className="btn icon-only empty-btn download-file"
                     onClick={() => {
-                      downloadFile(
-// 'https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/C01C_066_000041_20201124_A.txt?AWSAccessKeyId=ASIAQRLXWZJ2BOMGUEEY&Expires=1606283936&Signature=CmKrOw3hsXdFLLaTPGiuK37nfN0%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEGwaCXVzLWVhc3QtMSJHMEUCIQDhYEXP%2FRIkI%2Few7UGBio6GiswfkaE9y7kiKIOG%2Bp%2FF8wIgL0O1lO7DR3zP5qGZ6VLBnb%2Fdki%2Bnv9%2B17%2Fuub%2BbJONQqlgMI5f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgwwMzcyOTUxNDc2MzYiDCQo%2F0l2j2C2tmbzxirqAs3BubpbWbKmGi%2BD7bVYvZ5%2BRTxHJB%2F8hYYgV4gvIQpj%2B8M8Zkms%2FKGpvw9cz50D0TnAwovRvg8J7yNRk3uSzx5J8BKk5F0RHfKB9JkD5dWZVQYxPaCfnxPgz%2Ftr11VYwB54JnZKRJ4MseGZpsNS2ka2ykH7u0%2BvvFFE11Xmel%2BwD%2Bj3FKXD%2FTPCMnIYirRV0aRaamJ8vjvOZOcq1NCl1MI%2BT91h3%2FPL04Y6AR497d%2B0UH4mYLiTbOBFLMTwIhlK4sD6wATuLnPfgiVCeuAwUko%2FEYbtkSMzgMPxFNhLD%2BTh4YYC7EdtpaHRtbTTSqo0QBSfE%2FpPL72aEd4hye7wlpWkav4BbNAQofWPr6voilBvcv%2Bmeyrnd%2BpfbFzrS5FSfm2n0p4vwr%2BPSB8aklM7tAWpP9MB7VEw8%2FYnM7PifXDK8VI9oeueRVhQk3UMidg%2BKKCKCjqshB1v1SmE%2BbWUdor%2FVwPWzEf6k0MWMN2s9%2F0FOqYBE8alS%2BHfgHvZLvTizKxN%2F4Ny7KGzCi0xRdQDRAffdCAREntDFIVLuLFDU2sz2v8Va6ykUI4YOHWvUfe8APQHnb2EYYG4RFSU9V6QpsJ%2B33JAURFDUA3QJTjpbZibLK6ymmDWaN6wmjlbKonQRRoRlyQg0OmG7Da7ncGHt6llKz%2BWcz8ADqGnfbOxgVVtSPXnFgG7mHAq90T8TjkTkSin5o1InJRBfw%3D%3D'
-                      'https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/DevTest1_006_20200616_C.txt?AWSAccessKeyId=ASIAQRLXWZJ2BOMGUEEY&Expires=1606286144&Signature=aagsQa6IQEUcxompTubj%2F7ANoYQ%3D&response-content-disposition=attachment&x-amz-security-token=IQoJb3JpZ2luX2VjEGwaCXVzLWVhc3QtMSJHMEUCIQDhYEXP%2FRIkI%2Few7UGBio6GiswfkaE9y7kiKIOG%2Bp%2FF8wIgL0O1lO7DR3zP5qGZ6VLBnb%2Fdki%2Bnv9%2B17%2Fuub%2BbJONQqlgMI5f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgwwMzcyOTUxNDc2MzYiDCQo%2F0l2j2C2tmbzxirqAs3BubpbWbKmGi%2BD7bVYvZ5%2BRTxHJB%2F8hYYgV4gvIQpj%2B8M8Zkms%2FKGpvw9cz50D0TnAwovRvg8J7yNRk3uSzx5J8BKk5F0RHfKB9JkD5dWZVQYxPaCfnxPgz%2Ftr11VYwB54JnZKRJ4MseGZpsNS2ka2ykH7u0%2BvvFFE11Xmel%2BwD%2Bj3FKXD%2FTPCMnIYirRV0aRaamJ8vjvOZOcq1NCl1MI%2BT91h3%2FPL04Y6AR497d%2B0UH4mYLiTbOBFLMTwIhlK4sD6wATuLnPfgiVCeuAwUko%2FEYbtkSMzgMPxFNhLD%2BTh4YYC7EdtpaHRtbTTSqo0QBSfE%2FpPL72aEd4hye7wlpWkav4BbNAQofWPr6voilBvcv%2Bmeyrnd%2BpfbFzrS5FSfm2n0p4vwr%2BPSB8aklM7tAWpP9MB7VEw8%2FYnM7PifXDK8VI9oeueRVhQk3UMidg%2BKKCKCjqshB1v1SmE%2BbWUdor%2FVwPWzEf6k0MWMN2s9%2F0FOqYBE8alS%2BHfgHvZLvTizKxN%2F4Ny7KGzCi0xRdQDRAffdCAREntDFIVLuLFDU2sz2v8Va6ykUI4YOHWvUfe8APQHnb2EYYG4RFSU9V6QpsJ%2B33JAURFDUA3QJTjpbZibLK6ymmDWaN6wmjlbKonQRRoRlyQg0OmG7Da7ncGHt6llKz%2BWcz8ADqGnfbOxgVVtSPXnFgG7mHAq90T8TjkTkSin5o1InJRBfw%3D%3D'
-                      )
+                      downloadFile([data.fileName]);
+
                     }}
             >
               <i className="icon fi flaticon-cloud-computing" />
@@ -77,9 +75,14 @@ const handleResponse = (response) => {
     const responseData = json.data;
     if (response.ok && responseData) {
       responseData.forEach((file, index) => {
-        let action = [FILE_SUCCESS];
+        let action = {
+          status: FILE_SUCCESS,
+          fileName: file.fileName
+        };
         if (file.fileName.endsWith(ERROR_FILE_EXTENSION)) {
-          action = [FILE_ERROR];
+          action = {
+            status: FILE_ERROR,
+          }
         }
         files.push({
           key: index + 1,
@@ -103,7 +106,40 @@ const fileListRequestHandler = () => fetch(getBffUrlConfig().listOutputFilesEndp
   }).then(handleResponse);
 
 
-const downloadFile  =  (url) => {
+const generateSignedUrls = (fileNamesArray) => fetch(getBffUrlConfig().outputBucketFilesSignedUrlEndpoint, {
+      method: 'POST',
+      body: JSON.stringify({
+          'fileNames': fileNamesArray
+        }),
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    });
+
+const downloadFile  =  (fileNamesArray) => {
+  console.log(fileNamesArray)
+
+  generateSignedUrls(fileNamesArray)
+      .then(response => {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json()
+      })
+      .then(response => {
+        console.log(response)
+        console.log(response.data)
+          const fileNameUrlArray = response.data;
+          console.log(fileNameUrlArray.length)
+          downloadFromSignedUrl(fileNameUrlArray)
+        }).catch(error => {
+  console.log(error)
+});
+
+
+
 //   fetch(url, {
 //         method: 'GET',
 //         headers: {
@@ -125,22 +161,7 @@ const ar = ['https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/C01C
 'https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/DevTest1_006_20200616_C.txt?AWSAccessKeyId=ASIAQRLXWZJ2BOMGUEEY&Expires=1606286144&Signature=aagsQa6IQEUcxompTubj%2F7ANoYQ%3D&response-content-disposition=attachment&x-amz-security-token=IQoJb3JpZ2luX2VjEGwaCXVzLWVhc3QtMSJHMEUCIQDhYEXP%2FRIkI%2Few7UGBio6GiswfkaE9y7kiKIOG%2Bp%2FF8wIgL0O1lO7DR3zP5qGZ6VLBnb%2Fdki%2Bnv9%2B17%2Fuub%2BbJONQqlgMI5f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgwwMzcyOTUxNDc2MzYiDCQo%2F0l2j2C2tmbzxirqAs3BubpbWbKmGi%2BD7bVYvZ5%2BRTxHJB%2F8hYYgV4gvIQpj%2B8M8Zkms%2FKGpvw9cz50D0TnAwovRvg8J7yNRk3uSzx5J8BKk5F0RHfKB9JkD5dWZVQYxPaCfnxPgz%2Ftr11VYwB54JnZKRJ4MseGZpsNS2ka2ykH7u0%2BvvFFE11Xmel%2BwD%2Bj3FKXD%2FTPCMnIYirRV0aRaamJ8vjvOZOcq1NCl1MI%2BT91h3%2FPL04Y6AR497d%2B0UH4mYLiTbOBFLMTwIhlK4sD6wATuLnPfgiVCeuAwUko%2FEYbtkSMzgMPxFNhLD%2BTh4YYC7EdtpaHRtbTTSqo0QBSfE%2FpPL72aEd4hye7wlpWkav4BbNAQofWPr6voilBvcv%2Bmeyrnd%2BpfbFzrS5FSfm2n0p4vwr%2BPSB8aklM7tAWpP9MB7VEw8%2FYnM7PifXDK8VI9oeueRVhQk3UMidg%2BKKCKCjqshB1v1SmE%2BbWUdor%2FVwPWzEf6k0MWMN2s9%2F0FOqYBE8alS%2BHfgHvZLvTizKxN%2F4Ny7KGzCi0xRdQDRAffdCAREntDFIVLuLFDU2sz2v8Va6ykUI4YOHWvUfe8APQHnb2EYYG4RFSU9V6QpsJ%2B33JAURFDUA3QJTjpbZibLK6ymmDWaN6wmjlbKonQRRoRlyQg0OmG7Da7ncGHt6llKz%2BWcz8ADqGnfbOxgVVtSPXnFgG7mHAq90T8TjkTkSin5o1InJRBfw%3D%3D']
 
 
-  ar.forEach(url => {
 
-    fetch(url)
-        .then(response => response.blob())
-        .then(blob => URL.createObjectURL(blob))
-        .then(uril => {
-          console.log(uril)
-          var link = document.createElement("a");
-          link.href = uril;
-          link.download = 'filename' + ".csv";
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-        });
-
-  });
 
   //                       const link = document.createElement('a');
 //                       link.href = "https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/C01C_066_000041_20201124_A.txt?AWSAccessKeyId=ASIAQRLXWZJ2LLLMGGFX&Expires=1606272121&Signature=25Vfm7wMDh4IczxVkqgL7oWPX74%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEGgaCXVzLWVhc3QtMSJHMEUCIFG%2BBchlV48bGulCYeJqUTa3NFr0I%2BIKFlcBQzHIurtFAiEAwrFJf9zcD0D5gDRse%2By0k%2FV9GmFHEWfFSP4b%2F6%2FvLTsqlgMI4f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgwwMzcyOTUxNDc2MzYiDJ5Sj8JgC5EA%2BQjOpCrqAsdWZTqAyo7HvTtbFQmiOAhNizaOCCClV5dDuKzpyUCwqWjvWy4eWJPUnNUYKxPeaO%2FuYeBg%2BlfSVT8UNsKbLvKQNUtbDVQp4i1wu6%2FEqz2Yft1x6L2vuLtEYnmmJAOJEmAiFoYLoop8sU9hHt2ab1C4wkwkZfE2AvdFfgm9zQ3nEOy1meU9HUW0Wj8r4cbuZNmt2JaTrYkAPrNz1Tf8luSPZtLJxjPTTFsKQPcV51%2FsrHVOZoT0bo5wgK4Ap9dwY2BjSGm%2FDe9JI6doWMSEf2svfK3RtUtMKo34PQKx6V%2FE8Jm8qhpa24kSLPgqHww2qdzlD3oB31O32V0auAjp9kqz9aLE1L0BxAzDiXSPId7Ae4mZIJMJw7X1LRKf3nZQ729Dt%2FOk5qyZIHWbcZWcBmSYkpbWeI4OrbGYG0G0P18%2BVE6nvMYi5CbMqgDbIPmpW6XIQ%2BaPBMv6NXoNwbu6xF9KQbYf831oBfJGMOe79v0FOqYBZJsqnU9cvt%2BE3EAvfzGOqh%2F0iI8xnSy8lDDU%2Be1ewZ%2BANMpCG%2Fn6Z00D%2FV2Q%2Bpsr%2BKM7egs4hB7X7BaiyjirK3YOFbwbmhxN5CDaMLoKuogpLDDaQ0y%2BTvXZA4bPZl%2Fxg3x7cocCH%2F7pt2tBQVYzF%2BDv5Yo7yGB3%2F1mKbonM2H1mcmI2INOf8z9cI4mhaPbg1kg9BzovmJBf58kqdffF%2BfP%2BUqKddw%3D%3D";
@@ -162,6 +183,37 @@ const ar = ['https://sysco-us-east-1-prcp-dev-batch-output.s3.amazonaws.com/C01C
 
 
 };
+
+const downloadFromSignedUrl = (fileNameUrlArray) => {
+  console.log('from signed', fileNameUrlArray)
+  fileNameUrlArray.forEach(({fileName, readUrl}) => {
+    console.log('filename', fileName)
+    console.log('readUrl', readUrl)
+    fetch(readUrl)
+        .then(response => {
+          console.log(response)
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response;
+        })
+        .then(response => response.blob())
+        .then(blob => URL.createObjectURL(blob))
+        .then(uril => {
+          console.log(uril)
+          let link = document.createElement("a");
+          link.href = uril;
+          link.download = fileName;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+  });
+}
 
 class FileList extends React.Component {
   constructor(props) {
--- src/utils/Configs.js
+++ src/utils/Configs.js
@@ -8,6 +8,7 @@ export const getBffUrlConfig = () => {
     const CONFIG = {};
     if (process.env.REACT_APP_ENV === 'dev') {
         CONFIG.listOutputFilesEndpoint = 'http://localhost:4000/local/v1/pci-bff/batch/files/output';
+        CONFIG.outputBucketFilesSignedUrlEndpoint = 'http://localhost:4000/local/v1/pci-bff/batch/signed-url/output';
         CONFIG.priceDataEndpoint = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/pricing/pricing-data';
         CONFIG.loginRedirectionUrl = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'https://cloud-pci-bff-dev.prcp-np.us-east-1.aws.sysco.net/v1/pci-bff/auth/logout';
@@ -32,6 +33,7 @@ export const getBffUrlConfig = () => {
         CONFIG.userDetailsUrl = 'https://cloud-pci-bff-prod.prcp.us-east-1.aws.sysco.net/v1/pci-bff/auth/user-details';
     } else {
         CONFIG.listOutputFilesEndpoint = 'http://localhost:4000/local/v1/pci-bff/batch/files/output';
+        CONFIG.outputBucketFilesSignedUrlEndpoint = 'http://localhost:4000/local/v1/pci-bff/batch/signed-url/output';
         CONFIG.priceDataEndpoint = 'http://localhost:4000/local/v1/pci-bff/pricing/pricing-data';
         CONFIG.loginRedirectionUrl = 'http://localhost:4000/local/v1/pci-bff/auth/login';
         CONFIG.logOutRedirectionUrl = 'http://localhost:4000/local/v1/pci-bff/auth/logout';
