90689079761044ce76a1fb0f429c894f9707b9e7
Chandima Samarasinghe <chandima.samarasinghe@syscolabs.com>
2020-12-04 14:08:34 +0530

    PRCP-2906 - PCI Grabber - File Search Support added

--- src/containers/FileUpload/FileList.js
+++ src/containers/FileUpload/FileList.js
@@ -101,6 +101,13 @@ const fileListRequestHandler = () => fetch(getBffUrlConfig().listOutputFilesEndp
     },
   }).then(handleResponse);
 
+const fileSearchListRequestHandler = (searchRequestEndpoint) => fetch(searchRequestEndpoint, {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'
+  },
+}).then(handleResponse);
 
 const downloadFile  =  (url) => {
   fetch(url, {
@@ -126,13 +133,15 @@ class FileList extends React.Component {
       selectedRowKeys: [], // Check here to configure the default column
       loading: false,
       data: [],
-      dataIsReturned : false
+      dataIsReturned : false,
+      searchString: ''
     };
   }
 
   loadDataFiles = () => {
     this.setState({
-      dataIsReturned : false
+      dataIsReturned : false,
+      searchString: ''
     })
     fileListRequestHandler().then((res) => {
       if(res.success) {
@@ -142,6 +151,29 @@ class FileList extends React.Component {
         })
       }
     });
+  }
+
+  getListSearchFilesEndpoint = (source, prefix) => {
+    return getBffUrlConfig().listSearchFilesEndpoint + source + "/" + prefix;
+  }
+
+  loadSearchDataFiles = (value) => {
+    if(value !== ''){
+      this.setState({
+        dataIsReturned : false,
+      })
+
+      fileSearchListRequestHandler(this.getListSearchFilesEndpoint("output", value)).then((res) => {
+        if(res.success) {
+          this.setState({
+            data: res.data,
+            dataIsReturned : true
+          })
+        }
+      });
+    }else{
+      this.loadDataFiles();
+    }
   }
 
   componentDidMount() {
@@ -163,8 +195,12 @@ class FileList extends React.Component {
     this.setState({ selectedRowKeys });
   };
 
+  onSearchStringChange = (searchBox) => {
+    this.setState({ searchString: searchBox.target.value });
+  };
+
   render() {
-    const {loading, selectedRowKeys, data, dataIsReturned} = this.state;
+    const {loading, selectedRowKeys, data, dataIsReturned, searchString} = this.state;
     const rowSelection = {
       selectedRowKeys,
       onChange: this.onSelectChange,
@@ -181,7 +217,9 @@ class FileList extends React.Component {
             <Search
                 placeholder="Search"
                 className="search-list"
-                // loading
+                value={searchString}
+                onChange={this.onSearchStringChange}
+                onSearch={this.loadSearchDataFiles}
             />
             <div className="spacer"></div>
             <div className="selected-item-status">
