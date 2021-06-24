ce9c7adaff2fdf53c94ed0d9942fafb851060bc7
rshe2081 <Rashmi.Shehana@syscolabs.com>
2021-06-24 15:48:44 +0530

    [TRSP-113 pzr helper]

---
+++ src/containers/PriceZoneReassignment/PriceZoneReassignmentHelper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Select } from 'antd';
+
+const { Option } = Select;
+
+export const getBusinessUnits = (businessUnitsMaps) => {
+    const businessUnitsMap = new Map([
+        ['011', { id: '011', shortName: 'Temp'}]
+      ]);
+
+    const businessUnitOptions = [];
+    if (businessUnitsMap) {
+        businessUnitsMap.forEach(((businessUnit) => {
+            businessUnitOptions.push(
+                <Option key={businessUnit.id} value={businessUnit.id}>{businessUnit.id} - {businessUnit.shortName}</Option>
+            );
+        }));
+    }
+    return businessUnitOptions;
+};
