b414f2bdd2c6886d1e9df7b9c1565a3b1b28cb52
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2021-06-25 12:27:10 +0530

    [TRSP-116] resolving merge conflicts

--- src/containers/PriceZoneReassignment/PriceZoneReview/PriceZoneReview.js
+++ src/containers/PriceZoneReassignment/PriceZoneReview/PriceZoneReview.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/display-name */
 import React, { useState, useEffect, useContext, useMemo } from 'react';
-import useModal from "../../../hooks/useModal";
 import { Table, Space, Spin } from 'antd';
+import useModal from "../../../hooks/useModal";
 import {getBffUrlConfig} from '../../../utils/Configs';
 import {
   formatPZRequest,
@@ -17,42 +17,6 @@ import AproveRejectButtons from './AproveRejectButtons';
 import CustomPagination from '../../../components/CustomPagination';
 import businessUnitMap from '../../../constants/BusinessUnits';
 
-const columns = [
-  {
-    title: 'SUBMITTED BY',
-    dataIndex: 'submission',
-    key: 'submission',
-    width: '20%',
-    render: (submission) => (
-      <Space size='middle'>
-        <ReviewSubmitter submission={submission} />
-      </Space>
-    ),
-  },
-  {
-    title: 'SUMMARY OF CHANGES',
-    dataIndex: 'changeSummary',
-    key: 'changeSummary',
-    width: '40%',
-    render: (changeSummary) => (
-      <Space size='middle'>
-        <ReviewSummery changeSummary={changeSummary} />
-      </Space>
-    ),
-  },
-  {
-    title: 'ACTION',
-    dataIndex: 'accept',
-    key: 'accept',
-    width: '20%',
-    render: (cell, row, index) => (
-      <Space size='middle'>
-        <AproveRejectButtons row={row} index={index}/>
-      </Space>
-    ),
-  },
-];
-
 export default function PriceZoneReview() {
   const [totalResultCount, setTotalResultCount] = useState(REVIEW_RESULT_TABLE_PAGE_SIZE);
   const [resultLoading, setResultLoading] = useState(false);
@@ -66,6 +30,8 @@ export default function PriceZoneReview() {
     }
     return [];
   }, [pzrContext.dataStore, pzrContext.currentPage]);
+
+  const { on, Modal, toggle } = useModal();
 
   const fetchPZChangeRequests = (page) => {
     const paginationParams = generatePaginationParams(page, REVIEW_RESULT_TABLE_PAGE_SIZE);
@@ -101,6 +67,7 @@ export default function PriceZoneReview() {
   };
 
   const loadTableData = (page = 1) => {
+    console.log('loading data');
     if (!pzrContext.dataStore[page]) {
       fetchPZChangeRequests(page);
     }
@@ -118,6 +85,65 @@ export default function PriceZoneReview() {
     loadTableData();
   }, []);
 
+  const columns = [
+    {
+      title: 'SUBMITTED BY',
+      dataIndex: 'submission',
+      key: 'submission',
+      width: '20%',
+      render: (submission) => (
+        <Space size='middle'>
+          <ReviewSubmitter submission={submission} />
+        </Space>
+      ),
+    },
+    {
+      title: 'SUMMARY OF CHANGES',
+      dataIndex: 'changeSummary',
+      key: 'changeSummary',
+      width: '40%',
+      render: (changeSummary) => (
+        <Space size='middle' onClick={toggle}>
+          <ReviewSummery changeSummary={changeSummary} />
+        </Space>
+      ),
+    },
+    {
+      title: 'ACTION',
+      dataIndex: 'accept',
+      key: 'accept',
+      width: '20%',
+      render: (cell, row, index) => (
+        <Space size='middle'>
+          <AproveRejectButtons row={row} index={index}/>
+        </Space>
+      ),
+    },
+  ];
+
+  const ReferenceTable = () => {
+    return (
+      <div>
+        {Modal(
+          {
+            title: "",
+            centered: "true",
+            onOK: () => toggle,
+            okText: "PROCEED",
+            cancelText: "CANCEL",
+            width:500
+          },
+
+          <div className="pz-confirm-pop-base">
+            <div className="pz-pop-table">
+            <Table columns={columns} dataSource={dataSource} />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
+
   const renderLoader = () => (
     <Space size="middle" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
       <Spin size="large" />
@@ -125,11 +151,14 @@ export default function PriceZoneReview() {
   );
 
   const renderDataTable = () => (
-    <Table
-      columns={columns}
-      dataSource={dataSource}
-      pagination={false}
-    />
+    <>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={false}
+      />
+      <ReferenceTable/>
+    </>    
   );
 
   return (
