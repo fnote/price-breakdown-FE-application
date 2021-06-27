3731fdef6644816a046e6b9888927d37c00fadfb
sithija <sithija.thewahettige@sysco.com>
2021-06-27 21:54:43 +0530

    [TRSP-108] cleaning

--- src/containers/UserDetailContext.js
+++ src/containers/UserDetailContext.js
@@ -27,7 +27,6 @@ const UserDetailContextProvider = (props) => {
         if (newUserDetails && newUserDetails.authorizedPricingTransformationEnabledBunitList) {
             const modifiedUserDetails = { ...newUserDetails, businessUnitMap: createBusinessUnitMap(newUserDetails)};
             const modifiedState = { ...newStateData, userDetails: modifiedUserDetails };
-            console.log(JSON.stringify(modifiedState))
             setUserDetails(modifiedState);
         } else {
             setUserDetails(newStateData);
