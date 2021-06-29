eb6396636d7cf8b89045b6b6845370bb83d1661d
Vihanga nivarthana <Vihanga.Nivarthana@syscolabs.com>
2021-06-30 00:23:20 +0530

    new styles added

--- src/containers/PriceZoneReassignment/PriceZoneReview/ReviewSubmitter.js
+++ src/containers/PriceZoneReassignment/PriceZoneReview/ReviewSubmitter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {FileTextFilled} from '@ant-design/icons';
 import { Tooltip } from 'antd';
 
 export default function ReviewSubmitter({ submission: { id, givenName, surname, createdTime }}) {
@@ -20,7 +19,7 @@ export default function ReviewSubmitter({ submission: { id, givenName, surname, 
            </div>
            <div className="pz-review-note">
                <Tooltip title="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, perferendis! Dignissimos corporis autem repellendus repudiandae officia in laboriosam nostrum corrupti." color="#fff" overlayClassName="pz-tooltip" overlayStyle={{color: '#000'}}>
-           <FileTextFilled />
+               <i className="icon fi flaticon-note"/>
            </Tooltip>
            </div>
 
--- src/containers/PriceZoneReassignment/PriceZoneReview/ReviewSummery.js
+++ src/containers/PriceZoneReassignment/PriceZoneReview/ReviewSummery.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ExclamationCircleOutlined} from '@ant-design/icons'
 
 export default function ReviewSummery({ changeSummary: {
   businessUnit,
@@ -10,46 +11,64 @@ export default function ReviewSummery({ changeSummary: {
   itemAttributeGroupId
 } }) {
   return (
-    <div className="pz-review-wrapper">
-      <div className="pz-review-sum-left">
-        <div className="pz-review-sum-left-pz">
-          <span>pricezone</span>
-          <div className="pz-review-sum-zone">
-            <div className="pz-zone-wrapper">
-              <div className="pz-zone-from-to-base">
-                {/* <div className="pz-zone-from">2</div> */}
-                {/* <div className="pz-zone-separator"></div> */}
-                <div className="pz-zone-to">{newPriceZone}</div>
-              </div>
+    <div className="pz-review-wrapper pz-cursor">
+    <div className="pz-review-sum-left">
+      <div className="pz-review-sum-left-pz">
+        <span className="pz-caps">pricezone</span>
+        <div className="pz-review-sum-zone">
+          <div className="pz-zone-wrapper">
+            <div className="pz-zone-from-to-base">
+              <div className="pz-zone-from">2</div>
+              <div className="pz-zone-separator"></div>
+              <div className="pz-zone-to">{newPriceZone}</div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="pz-review-sum-left-opco">
+        <span className="pz-caps">opco</span>
         <div className="pz-review-sum-left-opco">
-          <span>opco</span>
-          <div className="pz-review-sum-left-opco">
-            <div className="pz-review-opco">{businessUnit}</div>
-          </div>
+          <div className="pz-review-opco">{businessUnit}</div>
         </div>
       </div>
-      <div className="pz-review-sum-right">
-        <div className="pz-review-right-wrapper">
-          <div className="pz-right-top">
-            <div className="pz-effective-date-text">effective date</div>
-            <div className="pz-effective-date">{effectiveFromDate}</div>
+    </div>
+    <div className="pz-review-sum-right">
+      <div className="pz-review-right-wrapper">
+        <div className="pz-right-top">
+          <div className="pz-effective-date-text pz-caps">effective date</div>
+          <div className="pz-effective-date pz-main-text">{effectiveFromDate}</div>
+        </div>
+        <div className="pz-right-bottom">
+          <div className="pz-right-bottom-left">
+            {1 == 2 ? (
+              <>
+                <div className="pz-effective-date-text pz-caps">customer group</div>
+                <div className="pz-effective-date pz-customer-tag">
+                {customerGroup}
+                </div>
+                <div className="pz-customer-total">241 customers</div>
+              </>
+            ) : (
+              <>
+                <div className="pz-effective-date-text pz-caps ">customer</div>
+                <div className="pz-effective-date">
+                  055437
+                </div>
+               
+              </>
+            )}
           </div>
-          <div className="pz-right-bottom">
-              <div className="right-bottom-left">
-              <div className="pz-effective-date-text ">customer group</div>
-            <div className="pz-effective-date pz-customer-tag">{customerGroup}</div>
-              </div>
-              <div className="right-bottom-right">
-              <div className="pz-effective-date-text">attribute group</div>
-            <div className="pz-effective-date pz-attribute-tag">{itemAttributeGroup}</div>
-              </div>
-            
+          <div className="pz-right-bottom-right">
+          
+            <div className="pz-effective-date-text pz-caps">attribute group</div>
+            <div className="pz-attrib-base">
+            <div className="pz-effective-date pz-attribute-tag">{itemAttributeGroup}</div> <ExclamationCircleOutlined className="pz-attrib-info" />
+            </div>
+            <div className="pz-attrib-total">341 items</div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
