3c00da706801c2ebd73872303be5016a56c7f549
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2021-07-02 14:46:27 +0530

    [TRSP-132] Fix useModal hook code styles

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,45 +1,47 @@
-
-
-import React, {useState, useCallback} from 'react'
-import {Modal as AntdModal} from 'antd'
-
+import React, { useState, useCallback } from 'react';
+import { Modal as AntdModal } from 'antd';
 
 const useModal = () => {
-  const [on, setOn] = useState(false)
-  const toggle = useCallback(() => setOn(!on), [on])
+  const [on, setOn] = useState(false);
+  const toggle = useCallback(() => setOn(!on), [on]);
   
-  const Modal = useCallback(
-    ({onOK,onCancel,noCancel , noOk ,still, ...rest}, child) => {
-      return (
-        <AntdModal
-        className="pz-antModal"
-          {...rest}
-          visible={on}
-          cancelButtonProps={{ style: noCancel && { display: 'none' } }}
-          okButtonProps={{ style: noOk && { display: 'none' } }}
-         
-          onOk={() => {
-            onOK && onOK()
-            {still ? setOn(true) : toggle()}
-          }}
+  const Modal = useCallback(({ onOK, onCancel, noCancel, noOk, still, ...rest }, child) => (
+    <AntdModal
+      className="pz-antModal"
+      {...rest}
+      visible={on}
+      cancelButtonProps={{ style: noCancel && { display: 'none' } }}
+      okButtonProps={{ style: noOk && { display: 'none' } }}
+    
+      onOk={() => {
+        if (onOK) {
+          onOK();
+        }
+        if (still) {
+          setOn(true);
+        } else {
+          toggle();
+        }
+      }}
 
-          onCancel={() => {
-            onCancel && onCancel()
-            toggle()
-          }}
-        
-        >
-          {child}
-        </AntdModal>
-      )
-    },
-    [on, toggle],
-  )
+      onCancel={() => {
+        if (onCancel) {
+          onCancel();
+        }
+        toggle();
+      }}
+    
+    >
+      {child}
+    </AntdModal>
+  ),
+  [on, toggle]);
+
   return {
     on,
     toggle,
     Modal,
-  }
-}
+  };
+};
 
 export default useModal;
