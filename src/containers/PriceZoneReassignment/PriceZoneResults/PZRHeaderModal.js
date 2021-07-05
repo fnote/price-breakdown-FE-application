42ac2930cb1a3253de02534204e0a28f8da35c45
rshe2081 <Rashmi.Shehana@syscolabs.com>
2021-07-05 19:37:38 +0530

    [TRSP-113]- fix UI Issue in change priceZone attribute confirmation text

--- src/containers/PriceZoneReassignment/PriceZoneResults/PZRHeaderModal.js
+++ src/containers/PriceZoneReassignment/PriceZoneResults/PZRHeaderModal.js
@@ -26,7 +26,7 @@ export const ModalComponent = ({Modal, setSubmitModal}) => (
                 <div className="pz-alert-main">Confirm Price Zone Change</div>
                 <div className="pz-alert-sub">
                     While performing this change, price zone for all the items
-                    associated with item attribute group and all the customers
+                    associated with attribute group and all the customers
                     associated with customer group will be updated.
                 </div>
             </div>
