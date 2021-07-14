4cac8c5faff873d7a4f4886d96704b393d3fb209
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2021-07-14 17:08:15 +0530

    [TRSP-148] code reformat

--- src/components/ScrollTable.js
+++ src/components/ScrollTable.js
@@ -1,55 +1,49 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Table } from "antd";
+import React, { useEffect, useState, useRef } from 'react';
+import { Table } from 'antd';
 
-
-
-export function getTableScroll({ extraHeight, id, ref } = {}) {
-  if (typeof extraHeight == "undefined") {
-    extraHeight = 74;
-  }
+export function getTableScroll({ extraHeight = 74, id, ref } = {}) {
   let tHeader = null;
   if (ref && ref.current) {
-    tHeader = ref.current.getElementsByClassName("ant-table-thead")[0];
+    tHeader = ref.current.getElementsByClassName('ant-table-thead')[0];
   } else if (id) {
     tHeader = document.getElementById(id)
-      ? document.getElementById(id).getElementsByClassName("ant-table-thead")[0]
+      ? document.getElementById(id).getElementsByClassName('ant-table-thead')[0]
       : null;
   } else {
-    tHeader = document.getElementsByClassName("ant-table-thead")[0];
+    tHeader = document.getElementsByClassName('ant-table-thead')[0];
   }
 
   let tHeaderBottom = 0;
   if (tHeader) {
     tHeaderBottom = tHeader.getBoundingClientRect().bottom;
   }
+  const height = `calc(100vh - ${tHeaderBottom + extraHeight}px)`;
   if (ref && ref.current) {
-    let placeholder = ref.current.getElementsByClassName(
-      "ant-table-placeholder"
+    const placeholder = ref.current.getElementsByClassName(
+      'ant-table-placeholder'
     )[0];
     if (placeholder) {
       placeholder.style.height = height;
-      if (placeholder.tagName === "DIV") {
-        placeholder.style.display = "flex";
-        placeholder.style.alignItems = "center";
-        placeholder.style.justifyContent = "center";
+      if (placeholder.tagName === 'DIV') {
+        placeholder.style.display = 'flex';
+        placeholder.style.alignItems = 'center';
+        placeholder.style.justifyContent = 'center';
       }
     }
   }
-
-  var height = `calc(100vh - ${tHeaderBottom + extraHeight}px)`;
   return height;
 }
 
 export default function ScrollTable(props) {
-    const [scrollY, setScrollY] = useState();
-    let countRef = useRef(null);
-    useEffect(() => {
-      let scrolly = getTableScroll({ ref: countRef });
-      setScrollY(scrolly);
-    }, [props]);
-    return (
-      <div ref={countRef}>
-        <Table {...props} scroll={{ x: false, y: scrollY }} />
-      </div>
-    );
-  }
+  const [scrollY, setScrollY] = useState();
+  const countRef = useRef(null);
+  useEffect(() => {
+    const scrolly = getTableScroll({ ref: countRef });
+    setScrollY(scrolly);
+  }, [props]);
+  return (
+    <div ref={countRef}>
+      <Table {...props} scroll={{ x: false, y: scrollY }} />
+    </div>
+  );
+}
