bf6436ae5139a24e897354803ef4b020934f67cf
Tharuka Jayalath <tharukajayalath@syscolabs.com>
2021-10-27 17:21:58 +0530

    [TRSP-278] Add component ids
    Add ids to newly added components with txn logs changes so that ui automation can be done easily

--- src/containers/PriceZoneReassignment/PriceZoneLog/PriceZoneStatus.js
+++ src/containers/PriceZoneReassignment/PriceZoneLog/PriceZoneStatus.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function PriceZoneStatus({status}) {
 
 const Approved = ()=>{
-    return(<div id="pz-log-approved" className="pz-log-status-tag pz-log-approved">APPROVED</div>)
+    return(<div id="pz-log-status" className="pz-log-status-tag pz-log-approved">APPROVED</div>)
 }
 const Rejected = ()=>{
-    return(<div id="pz-log-rejected" className="pz-log-status-tag pz-log-rejected">REJECTED</div>)
+    return(<div id="pz-log-status" className="pz-log-status-tag pz-log-rejected">REJECTED</div>)
 }
 const Discarded = ()=>{
-    return(<div id="pz-log-discarded" className="pz-log-status-tag pz-log-discarded">DISCARDED</div>)
+    return(<div id="pz-log-status" className="pz-log-status-tag pz-log-discarded">DISCARDED</div>)
 }
   return (
       // ADDED FOR TESTING PURPOSES
