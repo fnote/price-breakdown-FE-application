59bee6a8fec2abea48e33def054b88e9d01acb4c
sithija <sithija.thewahettige@sysco.com>
2021-11-02 08:48:19 +0530

    [TRSP-278] fix

--- src/containers/PriceZoneReassignment/PriceZoneLog/PzPopover.js
+++ src/containers/PriceZoneReassignment/PriceZoneLog/PzPopover.js
@@ -4,8 +4,8 @@ import {fetchCustomerDetails} from "../handlers/PZRGetCustomerDetails";
 
 export default function PzPopover({id,customerGroup,customerAccount}) {
 
-    const [setResultLoading] = useState(false);
-    const [setError] = useState(false);
+    const [resultLoading, setResultLoading] = useState(false);
+    const [error, setError] = useState(false);
     const [customers, setCustomers] = useState(null);
 
     const loadPopOverWithCustomerData = ()=> fetchCustomerDetails({id, setResultLoading,setError, setCustomers});
